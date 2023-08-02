a70adf1fccd6aa0c7c3064e5b8040739739df0d5
Deekshith Shetty <deekshithshetty698@gmail.com>
2023-08-02 21:58:22 +0530

    Change Routing to Nested Routing

--- src/App.js
+++ src/App.js
@@ -10,9 +10,19 @@ function App() {
       <Routes>
         <Route path="/" element={<h1>HI</h1>} />
         <Route path="/about" element={<h1>This is about</h1>} />
+
+        {/* Normal Routing.
         <Route path="/books" element={<h1>This is books</h1>} />
         <Route path="/books/:id" element={<h1>Inside a book</h1>} />
-        <Route path="*" element={<h1>Invalid Page</h1>} />
+        <Route path="/books/new" element={<h1>Add a new book</h1>} />
+        <Route path="*" element={<h1>Invalid Page</h1>} /> */}
+
+        {/* Nested Routing */}
+        <Route path="/books">
+          <Route index element={<h1>This is Books</h1>} />
+          <Route path=":id" element={<h1>Inside a Book</h1>} />
+          <Route path="new" element={<h1>Add a new Book</h1>} />
+        </Route>
       </Routes>
     </>
   );
